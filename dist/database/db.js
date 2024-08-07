cb31249ee65d8b5e3070e92257f4855c0e588d67
anudeep-sh <anudeep@optigrit.in>
2024-08-07 16:46:39 +0000

    fixes

--- dist/database/db.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const knex_1 = require("knex");
-const database_connection_1 = require("./database.connection");
-const config = database_connection_1.default[process.env.NODE_ENV || "development"];
-const db = (0, knex_1.default)(config);
-exports.default = db;
-//# sourceMappingURL=db.js.map
--- dist/utilities/helper.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.generateUniqueId = void 0;
-const generateUniqueId = () => {
-    return Math.floor(1000000000 + Math.random() * 9000000000).toString();
-};
-exports.generateUniqueId = generateUniqueId;
-//# sourceMappingURL=helper.js.map
