11a609a3f324cb6a4002ee12529751d13de41294
anudeep-sh <anudeep@optigrit.in>
2024-08-26 21:22:26 +0530

    Delete src directory

--- src/types/types.ts
+++
@@ -1,6 +0,0 @@
-
-
-export interface INetwork{
-    registerController: (ctx:any)=>Promise<any>
-    joinController: (ctx:any)=> Promise<any>
-}
--- src/utilities/helper.ts
+++
@@ -1,3 +0,0 @@
-export const generateUniqueId = () => {
-    return Math.floor(1000000000 + Math.random() * 9000000000).toString();
-  };
