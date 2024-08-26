55b4e7b590bed25e47bc53085fb969e8949b0da8
anudeep-sh <anudeep@optigrit.in>
2024-08-26 21:24:17 +0530

    Add files via upload

---
+++ src/database/migrations/20240802182617_create-users-table.ts
@@ -0,0 +1,24 @@
+import type { Knex } from "knex";
+
+
+export async function up(knex: Knex): Promise<void> {
+    return knex.schema.createTable('users', function(table) {
+        table.uuid('id').primary(); // Primary Key
+        table.string('shortcode').unique().notNullable(); // Unique 6-digit code
+        table.string('name').notNullable();
+        table.string('emailId').unique().notNullable();
+        table.string('password').notNullable();
+        table.string('status').notNullable();
+        table.string('pan_number').nullable()
+        table.string('aadhar_number').nullable()
+        table.string('bank_account_number').nullable()
+        table.string('ifsc_code').nullable()
+        table.string('upi_linkedin_number').nullable()
+        table.timestamp('timestamp').defaultTo(knex.fn.now());
+      });
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+}
+
---
+++ src/database/migrations/20240814201116_create-user_quota-table.ts
@@ -0,0 +1,20 @@
+import type { Knex } from "knex";
+
+
+export async function up(knex: Knex): Promise<void> {
+    return knex.schema.createTable('user_quota', function(table) {
+        table.uuid('id').primary(); // Primary Key
+        table.uuid('user_id').references('id').inTable('users').notNullable().onDelete('CASCADE');
+        table.integer('level1_quota').notNullable().defaultTo(0);
+        table.integer('level2_quota').notNullable().defaultTo(0);
+        table.integer('level3_quota').notNullable().defaultTo(0);
+        table.integer('level4_quota').notNullable().defaultTo(0);
+        table.timestamp('created_at').defaultTo(knex.fn.now());
+        table.timestamp('updated_at').defaultTo(knex.fn.now());
+    });
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+}
+
